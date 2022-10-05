9de2968607c72c23df7bfd84c68d95c80a5652cb
Valentin Kirev <valentinkirev666@gmail.com>
2022-10-05 16:00:34 +0300

    Added views

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from petstagram.photos.views import *
+
 urlpatterns = [
     path('add/', add_photo, name='add photo'),
     path('<int:pk>/', details_photo, name='details photo'),
