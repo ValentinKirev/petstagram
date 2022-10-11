38f4d8ba4f15ae9f64fba833a083181152a17390
Valentin Kirev <valentinkirev666@gmail.com>
2022-10-11 17:21:53 +0300

    Added Pet Model

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Pet(models.Model):
+    NAME_MAX_LENGTH = 30
+
+    name = models.CharField(
+        max_length=NAME_MAX_LENGTH,
+        null=False,
+        blank=False
+    )
+
+    personal_photo = models.URLField(
+        null=False,
+        blank=False
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        null=False,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        return super().save(*args, **kwargs)
